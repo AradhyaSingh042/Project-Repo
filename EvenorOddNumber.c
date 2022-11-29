d01edfed5afed8e968e6d145cfe013abb83b07fa
Professor047 <119353694+Professor047@users.noreply.github.com>
2022-11-29 20:13:35 +0530

    Added a Print Statement
    "Hello C"

--- EvenorOddNumber.c
+++ EvenorOddNumber.c
@@ -7,12 +7,14 @@ int main()
     scanf("%d", &n);
     printf("The Number is %d\n", n);
     //This will check if the number is odd or even
-    if(n%2==0){   //This will check if the number is odd or even
+    if(n%2==0){   
         printf("%d is a Even Number\n");
     }
     else{
         printf("%d is a Odd Number\n");
     }
+
+    printf("Hello C");
 
     return 0;
 }
